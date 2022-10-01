cc38a151c05722755f8b257e6dbf8a5a0d915ff4
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-10-01 19:08:20 -0300

    chore: create `Reminder` model

---
+++ src/modules/reminders/models/Reminder.ts
@@ -0,0 +1,30 @@
+import type { Reminder as ReminderBase } from '@prisma/client';
+
+export { Reminder };
+export type { IReminder };
+
+interface IReminder extends ReminderBase {}
+
+class Reminder implements IReminder {
+  constructor(private readonly data: IReminder) {}
+
+  get id() {
+    return this.data.id;
+  }
+
+  get description() {
+    return this.data.description;
+  }
+
+  get city() {
+    return this.data.city;
+  }
+
+  get date() {
+    return this.data.date;
+  }
+
+  get created_at() {
+    return this.data.created_at;
+  }
+}
